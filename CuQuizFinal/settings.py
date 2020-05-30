6f66c7fc1518bbdc6577131c0e122a028a191692
Gio gulisashvili <gulisashvili.gg@gmail.com>
2020-05-30 16:35:43 +0400

    დიზაინის გალამაზება 3

--- CuQuizFinal/settings.py
+++ CuQuizFinal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@p_wcmc_2&zdjq$-4xf0-2zq6#48m0oze7(ugt#)jc=7mrost8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.10', '127.0.0.1', 'localhost']
 
 # Application definition
 
