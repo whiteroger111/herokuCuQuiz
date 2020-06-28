686da97eac6adc264951974ab9f2d9ecd1329a19
nikaTukhashvili <nika.tukhashvili@yahoo.com>
2020-06-28 15:26:18 +0400

    facebook share link changed

--- classroom/views/students.py
+++ classroom/views/students.py
@@ -121,7 +121,7 @@ def take_quiz(request, pk):
     student = request.user.student
 
     if student.quizzes.filter(pk=pk).exists():
-        return render(request, 'students/taken_quiz.html')
+        return render(request, 'classroom/students/taken_quiz_list.html')
 
     total_questions = quiz.questions.count()
     unanswered_questions = student.get_unanswered_questions(quiz)
